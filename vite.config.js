515e793fffecd450a3730f1c3a0a74d7c32327db
karam Do <k21r21m21@gmail.com>
2024-08-19 01:11:59 +0300

    add deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: "/hangingman/",
     server: {
         open: true,
         port: 5500,
